288b04385182fce7a66a4d90110398657d604912
dmorton714 <102087408+dmorton714@users.noreply.github.com>
2024-07-17 15:25:40 -0400

    solution

--- string2.py
+++ string2.py
@@ -9,8 +9,12 @@
 # Return the resulting string.
 
 def verbing(s):
-    # +++your code here+++
-    return
+    if len(s) >= 3:
+        if s[-3:] != 'ing':
+            s = s + 'ing'
+        else:
+            s = s + 'ly'
+    return s
 
 
 # E. not_bad
@@ -23,8 +27,11 @@ def verbing(s):
 # This dinner is good!
 
 def not_bad(s):
-    # +++your code here+++
-    return
+    n = s.find('not')
+    b = s.find('bad')
+    if n != -1 and b != -1 and b > n:
+        s = s[:n] + 'good' + s[b+3:]
+    return s
 
 
 # F. front_back
@@ -34,14 +41,19 @@ def not_bad(s):
 # e.g. 'abcde', the front half is 'abc', the back half 'de'.
 # Given 2 strings, a and b, return a string of the form
 #  a-front + b-front + a-back + b-back
-
 def front_back(a, b):
-    # +++your code here+++
-    return
+    a_middle = len(a) // 2
+    b_middle = len(b) // 2
+    if len(a) % 2 == 1:
+        a_middle = a_middle + 1
+    if len(b) % 2 == 1:
+        b_middle = b_middle + 1
+    return a[:a_middle] + b[:b_middle] + a[a_middle:] + b[b_middle:]
 
 
 # Simple provided test() function used in main() to print
 # what each function returns vs. what it's supposed to return.
+
 def test(got, expected):
     if got == expected:
         prefix = ' OK '
@@ -52,6 +64,7 @@ def test(got, expected):
 
 # main() calls the above functions with interesting inputs,
 # using the above test() to check if the result is correct or not.
+
 def main():
     print('verbing')
     test(verbing('hail'), 'hailing')
